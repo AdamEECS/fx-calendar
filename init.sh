fd9ee2fcefc32a0df9cd06b368d4de45a7aa57e9
suzumiya <jrgwyz@163.com>
2017-06-19 14:47:30 +0800

    sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
-ln -s /var/www/md-resume/config/supervisor.conf /etc/supervisor/conf.d/md-resume.conf
+ln -s /var/www/fx-calendar/config/supervisor.conf /etc/supervisor/conf.d/fx-calendar.conf
 
-ln -s /var/www/md-resume/config/nginx.conf /etc/nginx/sites-enabled/md-resume
+ln -s /var/www/fx-calendar/config/nginx.conf /etc/nginx/sites-enabled/fx-calendar
 
 pip3 install -r requirements.txt
